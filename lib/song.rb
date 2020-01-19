ab59eccdf669a5f5671eee0c5f341e3f63df566e
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-19 19:59:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,31 +11,12 @@ class Song
 
   @@all = []
 
-  #def initialize
-  #  self.class.all << self
-  #end
-
-  #def self.find_by_name(name)
-  #  @@songs.detect{|a| a.name == name}
-  #end
-
   def self.all
     @@all
   end
-
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
 
   def artist=(artist)
     @artist = artist
   end
 
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
 end
